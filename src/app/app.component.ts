4b2fb530c361fcaaf84ca62bfbcf66ef8eaa3044
killiannh <killiannhervagault@gmail.com>
2024-10-23 18:16:06 +0200

    added bootstrap & a few components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NavbarComponent } from "./navbar/navbar.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+  { path: 'profiles', component: ProfilesComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
