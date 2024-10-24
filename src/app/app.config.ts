3551db465cd17ac7cea12fdfec85e389cfb627b5
killiannh <killiannhervagault@gmail.com>
2024-10-24 18:09:28 +0200

    editing profiles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,11 @@ import { authInterceptor } from './auth.interceptor';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideNgIconsConfig } from '@ng-icons/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor])),
+  provideNgIconsConfig({
+    size: '1em',
+  })]
 };
--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { Profiles } from '../types';
 import { DataService } from '../data.service';
 import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapPencilFill, bootstrapTrashFill } from '@ng-icons/bootstrap-icons';
+
 
 @Component({
   selector: 'app-profiles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIconComponent],
+  providers: [provideIcons({ bootstrapPencilFill, bootstrapTrashFill })],
   templateUrl: './profiles.component.html',
   styleUrl: './profiles.component.css'
 })
